3752f71b5422f1e33720f6416b01b00001b7fc9b
Ali Sadikin <alisadikin008@gmail.com>
2021-05-18 15:46:21 +0700

    check whether file exist or no, when exist execute file else from command line

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import {processLineByLine} from './helper'
+import {processLineByLine, generateCommandsFromInputLines} from './helper'
+import { ReadLine, createInterface } from "readline";
 
 const fileInput = process.argv.slice(2)
+const readLine = createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+});
+
 if (!fileInput.length) {
-    throw new Error(`Missing param file_inputs.txt`)
+    readLine.on('line', function (line) {
+        generateCommandsFromInputLines(line)
+    });
+}else{
+    processLineByLine(`${fileInput}`).catch((e) => {
+        console.log(e)
+    })
 }
-processLineByLine(`${fileInput}`).catch((e) => {
-    console.log(e)
-})
+
+
+
+
+
